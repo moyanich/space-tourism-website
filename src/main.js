2f594f1b9197fd7e97e1fb74d82159fb73dcad00
Amoy N <moyanich@users.noreply.github.com>
2022-03-16 17:01:15 -0500

    Mobile setup

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-
-/*
-$( 'ul.menu a' ).on( 'click', function () {
-      console.log('clicked');
-	$( 'ul.menu' ).find( 'li a.active' ).removeClass( 'active' );
-	$( this ).parent( 'li a' ).addClass( 'active' );
-});
-var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/")+1);
-$("ul.menu > li > a").each(function(){
-      if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
-      $(this).addClass("active");
-      // $(this).parent("li").addClass("active");
-}) 
-
-*/
-
 jQuery(function($) {
       var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
       $('ul.menu a').each(function() {
@@ -22,9 +6,5 @@ jQuery(function($) {
             }
       });
 });
-
-
-
-
 
 
