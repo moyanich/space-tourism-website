f282dd5b76007da8d04ad22f850d88bdf097eab0
Amoy N <moyanich@users.noreply.github.com>
2022-03-16 10:36:37 -0500

    Setup typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
 			fontFamily: {
 				'barlow': ["Barlow Condensed", "sans-serif"],
 				'bellefair': ["Bellefair", "serif"]
+			},
+			fontSize: {
+				'h3md': '3.5rem',
+				'h2md': '6.25rem',
+				'h9xl': '9.375rem',
 			}
 		},
+		
+		
 	},
 	plugins: [],
 }
